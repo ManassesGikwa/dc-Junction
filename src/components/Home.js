d9ee4cc2a2924ba5e3751abd8686aecea024791a
ManassesGikwa <manassesgikwa@gmail.com>
2024-06-28 20:31:00 +0300

    Homepage Edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div>
       <h2>Welcome to Deliverance Church Dagoretti Junction</h2>
-      <p>Thank you for visiting our website. Explore the various sections to learn more about our church and ministries.</p>
+      <p>We are under construction.</p>
     </div>
   );
 }
