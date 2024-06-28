0afb640d7bdd88feaf006f5a239ed17ac64da9bc
ManassesGikwa <manassesgikwa@gmail.com>
2024-06-28 19:54:43 +0300

    Set homepage for GitHub Pages deployment

---
+++ build/precache-manifest.f5dc77ebbaecbd7e3a19a4d7c2d9f411.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d4ee7559539d26e1c3bbe24ef13732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5541250b18d6253a1616",
+    "url": "/static/css/2.e1fb8b17.chunk.css"
+  },
+  {
+    "revision": "21fe282455ae1657417d",
+    "url": "/static/css/main.d1ec6710.chunk.css"
+  },
+  {
+    "revision": "5541250b18d6253a1616",
+    "url": "/static/js/2.44adad61.chunk.js"
+  },
+  {
+    "revision": "0a0b602d25062375dacd",
+    "url": "/static/js/3.40a56a06.chunk.js"
+  },
+  {
+    "revision": "21fe282455ae1657417d",
+    "url": "/static/js/main.46433ab9.chunk.js"
+  },
+  {
+    "revision": "42fa70b8bfc53216255b",
+    "url": "/static/js/runtime~main.5379b062.js"
+  },
+  {
+    "revision": "d661625f2042dda303724fc2edc5f0b7",
+    "url": "/static/media/Being Ready for Christ Pst manana.d661625f.jpg"
+  },
+  {
+    "revision": "0b98de23b3103f2775d3a64a2edb04db",
+    "url": "/static/media/Breaking Energy Suckers.0b98de23.jpg"
+  },
+  {
+    "revision": "5162ab3bee9fcbc4b570cd34ceb2d2f8",
+    "url": "/static/media/Breaking the Spirit of Hopelessness.5162ab3b.jpg"
+  },
+  {
+    "revision": "c4aa918374f4eee14282c29a0c4245d5",
+    "url": "/static/media/Bringing People to Jesus Rev James Gikwa.c4aa9183.jpg"
+  },
+  {
+    "revision": "aba1cc6824dc71094b7cfa23618ec963",
+    "url": "/static/media/Divine Acceleration Rev James Gikwa.aba1cc68.jpg"
+  },
+  {
+    "revision": "f9d3ac0e40b817d0bc46b7485fb6c906",
+    "url": "/static/media/Family communion service Rev James Gikwa.f9d3ac0e.jpg"
+  },
+  {
+    "revision": "7013169ca762aa1553201e4e5f8a4288",
+    "url": "/static/media/Getting strength in difficult times Rev Gikwa.7013169c.jpg"
+  },
+  {
+    "revision": "a97c2790f33c2457549c7df699e8153f",
+    "url": "/static/media/Keys to Threshing Mountains Rev James GIkwa.a97c2790.jpg"
+  },
+  {
+    "revision": "1927541d42cbdbe021f098178f4239ce",
+    "url": "/static/media/Serving God Manasses Gikwa.1927541d.jpg"
+  },
+  {
+    "revision": "7c6ce0a822fe7ff0f695461ab105bfd7",
+    "url": "/static/media/The Call of God with Eben.7c6ce0a8.jpg"
+  },
+  {
+    "revision": "40211424ab56d58f0b3fd1771d66682a",
+    "url": "/static/media/The Living Hope Rev James Gikwa.40211424.jpg"
+  },
+  {
+    "revision": "c85ea3a098e37f8c90f856ff15858852",
+    "url": "/static/media/The Power of Praise.c85ea3a0.jpg"
+  },
+  {
+    "revision": "195a70568222fee23c2e6f2490b59a85",
+    "url": "/static/media/The unchanging God With Rev James Gikwa.195a7056.jpg"
+  },
+  {
+    "revision": "9a485268774d8bbdc315fceab846e8a3",
+    "url": "/static/media/This Time Rev Gikwa.9a485268.jpg"
+  },
+  {
+    "revision": "f91551541a198ef2d982cb640d2d606f",
+    "url": "/static/media/Threshing MOuntains by the Power Within us.f9155154.jpg"
+  },
+  {
+    "revision": "2f38b43d896b7f056a0275dc040fbf1f",
+    "url": "/static/media/Threshing Mountains by the power of love.2f38b43d.jpg"
+  },
+  {
+    "revision": "d748d66fde4703ef8e86d68d2e15d63e",
+    "url": "/static/media/Triumphant Service Rev James Gikwa.d748d66f.jpg"
+  },
+  {
+    "revision": "5b6a919b7acc39dd924462359b57323f",
+    "url": "/static/media/holy communion Rev James Gikwa.5b6a919b.jpg"
+  }
+]);
